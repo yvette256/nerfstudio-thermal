1a9fdbbeca8384c91df4b7aa1c53049dff113fdf
Yvette Lin <yvette.lrox1@gmail.com>
2023-12-07 20:14:11 -0800

    Can run flir_utils as script to extract raw rgb/thermal from flir

--- nerfstudio/process_data/flir_utils.py
+++ nerfstudio/process_data/flir_utils.py
@@ -310,23 +310,7 @@ def extract_raws_from_dir(in_path, out_path=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract and visualize Flir Image data')
-    parser.add_argument('-i', '--input', type=str, help='Input image. Ex. img.jpg', required=True)
-    parser.add_argument('-p', '--plot', help='Generate a plot using matplotlib', required=False, action='store_true')
-    parser.add_argument('-exif', '--exiftool', type=str, help='Custom path to exiftool', required=False,
-                        default='exiftool')
-    parser.add_argument('-csv', '--extractcsv', help='Export the thermal data per pixel encoded as csv file',
-                        required=False)
-    parser.add_argument('-d', '--debug', help='Set the debug flag', required=False,
-                        action='store_true')
+    parser.add_argument('in_dir', type=str, help='Directory of input FLIR images')
+    parser.add_argument('out_dir', type=str, help='Directory to store extracted output')
     args = parser.parse_args()
-
-    fie = FlirImageExtractor(exiftool_path=args.exiftool, is_debug=args.debug)
-    fie.process_image(args.input)
-
-    if args.plot:
-        fie.plot()
-
-    if args.extractcsv:
-        fie.export_thermal_to_csv(args.extractcsv)
-
-    fie.save_images()
+    extract_raws_from_dir(args.in_dir, out_path=args.out_dir)
